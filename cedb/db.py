2941b2b69084851d6d8ca94975e9acb4a031b508
Mengwei Shi <33439014+shimw6828@users.noreply.github.com>
2021-09-27 16:06:55 +0800

    mask passwd

--- cedb/db.py
+++ cedb/db.py
@@ -1,5 +1,5 @@
 from flask_pymongo import PyMongo
 from cedb import app
 
-app.config["MONGO_URI"] = "mongodb://cedb_reader:cedb_reader_access@localhost:27017/CEdb"
+app.config["MONGO_URI"] = "mongodb://cedb_reader:passwdxxxxxx@localhost:xxxxx/CEdb"
 mongo = PyMongo(app)
